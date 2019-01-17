424c9cb765e26d06587b996a7f30ebe047f378d3
dbc2201 <divyanshb22@gmail.com>
2019-01-17 09:03:55 +0530

    add queue main class

--- src/main/QueueADT.java
+++ src/main/QueueADT.java
@@ -3,4 +3,46 @@ package main;
 public class QueueADT
 {
 
+	int[] queue = new int[10];
+	int front = -1;
+	int rear = -1;
+	int size = queue.length;
+
+	void enqueue(int value)
+	{
+
+	}
+
+	int dequeue()
+	{
+		int deleted = 0;
+
+
+		return deleted;
+	}
+
+	int peek()
+	{
+		int item = 0;
+
+		return item;
+	}
+
+	int size()
+	{
+		int size = 0;
+
+		return size;
+	}
+
+	boolean isEmpty()
+	{
+		return false;
+	}
+
+	boolean isFull()
+	{
+		return false;
+	}
+
 }
