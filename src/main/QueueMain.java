c26a834a5135ec8bca3f15075a0088e52ebdfc06
dbc2201 <divyanshb22@gmail.com>
2019-01-17 09:04:07 +0530

    add main method

---
+++ src/main/QueueMain.java
@@ -0,0 +1,14 @@
+package main;
+
+public class QueueMain
+{
+	public static void main(String[] args)
+	{
+		QueueADT queue1 = new QueueADT();
+
+//		TODO add method call for enqueue
+
+//		TODO add method call for dequeue
+
+	}
+}
